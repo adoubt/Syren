ad0d35fc19beb8bc1b0cf7f9a0d03d0d88ba41f7
goldenkernel <124398859+adoubt@users.noreply.github.com>
2024-11-08 09:14:55 +0000

    env

---
+++ src/misc.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN=os.getenv('BOT_TOKEN')
+MANIFEST_URL =os.getenv('MANIFEST_URL')
+SUPER_ADMIN = os.getenv('SUPER_ADMIN')
+PASSWORD = os.getenv('PASSWORD') # /set_admin{PASSWORD}
+
+
+bot_id = BOT_TOKEN.split(":",1)[0]
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
+
+
+#AAIO   
+AAIO_TOKEN = os.getenv('AAIO_TOKEN')
+MERCHANT_ID = os.getenv('MERCHANT_ID') # ID магазина
+SECRET = os.getenv('SECRET') # Секретный ключ №1 из настроек магазина
+SECRET2 = os.getenv('SECRET2') # Секретный ключ №2 из настроек магазина
