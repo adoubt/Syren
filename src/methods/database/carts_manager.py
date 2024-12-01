c46f11407e35595044f27e0edc752c54aa21887f
adoubt <gerya1508@gmail.com>
2024-12-01 21:44:28 +0700

    rename cart items, products

--- src/methods/database/carts_manager.py
+++ src/methods/database/carts_manager.py
@@ -2,6 +2,7 @@ import aiosqlite
 from typing import List, Tuple, Optional
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
@@ -9,23 +10,24 @@ class Cart:
     cart_id: int
     user_id: int
     status: str
-    
+    created_at :datetime
+    updated_at:datetime
 
 
 @dataclass
 class CartItem:
-    cart_item_id: int
+    item_id: int
     cart_id: int
     product_id: int
     quantity: int
     license_id: int
-
+    added_at:datetime
 
 @dataclass
-class AppliedCouponCode:
+class AppliedCoupon:
     cart_id: int
     coupon_id:int
-
+    applied_at:datetime
 
 class Database:
     """Асинхронный класс для работы с базой данных."""
@@ -69,8 +71,8 @@ class Database:
                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
                              updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ''') 
             
-            await db.execute(''' CREATE TABLE IF NOT EXISTS carts_items (
-                              cart_item_id INTEGER PRIMARY KEY, 
+            await db.execute(''' CREATE TABLE IF NOT EXISTS items (
+                              item_id INTEGER PRIMARY KEY, 
                              cart_id INTEGER NOT NULL, 
                              product_id INTEGER NOT NULL, 
                              quantity INTEGER NOT NULL DEFAULT 1, 
@@ -135,7 +137,7 @@ class CartItemManager:
     async def add_item_to_cart(self, cart: Cart, product_id: int, license_id: int, quantity: int = 1) -> None:
         """Добавить товар в корзину."""
         await self.db.execute(
-            'REPLACE INTO carts_items (cart_id, product_id, quantity, license_id) VALUES (?, ?, ?, ?)', 
+            'REPLACE INTO items (cart_id, product_id, quantity, license_id) VALUES (?, ?, ?, ?)', 
             (cart.cart_id, product_id, quantity, license_id)
         )
 
@@ -143,14 +145,14 @@ class CartItemManager:
     async def remove_item_from_cart(self, cart: Cart, product_id: int) -> None:
         """Удалить товар из корзины."""
         await self.db.execute(
-            'DELETE FROM carts_items WHERE cart_id = ? AND product_id = ?', 
+            'DELETE FROM items WHERE cart_id = ? AND product_id = ?', 
             (cart.cart_id, product_id)
         )
 
     async def get_items_from_cart(self, cart: Cart) -> List[CartItem]:
         """Получить все товары из корзины."""
         rows = await self.db.fetch_all( 
-            'SELECT cart_item_id, cart_id, product_id, quantity, license_id, added_at FROM carts_items WHERE cart_id = ?', 
+            'SELECT item_id, cart_id, product_id, quantity, license_id, added_at FROM items WHERE cart_id = ?', 
             (cart.cart_id,)
         )
         return [CartItem(*row) for row in rows]
@@ -158,26 +160,26 @@ class CartItemManager:
     async def clear_cart(self, cart: Cart) -> None:
         """Очистить корзину."""
         await self.db.execute(
-            'DELETE FROM carts_items WHERE cart_id = ?', 
+            'DELETE FROM items WHERE cart_id = ?', 
             (cart.cart_id,)
         )
 
     async def count_items_in_cart(self, cart: Cart) -> int: 
         """Посчитать количество товаров в корзине.""" 
         result = await self.db.fetch_one(
-             'SELECT COUNT(*) FROM carts_items WHERE cart_id = ?', 
+             'SELECT COUNT(*) FROM items WHERE cart_id = ?', 
              (cart.cart_id,) ) 
         return result[0] if result else 0
 
     async  def check_item_in_cart(self, cart:Cart, product_id: int)-> CartItem:
         """Найти совпадение по product_id"""
         cart_item = await self.db.fetch_all(
-            'SELECT * FROM carts_items WHERE product_id = ? AND cart_id = ?',
+            'SELECT * FROM items WHERE product_id = ? AND cart_id = ?',
             (product_id,cart.cart_id,)
         )
         return cart_item if cart_item else None
 
-class AppliedCouponCodeManager:
+class AppliedCouponManager:
     """Сервис для работы с Coupon Codes."""
     
     def __init__(self, db: Database):
@@ -198,7 +200,7 @@ class ShoppingCartService:
         self.db = Database()
         self.cart_manager = CartManager(self.db)
         self.cart_item_manager = CartItemManager(self.db)
-        self.applied_coupon_manager = AppliedCouponCodeManager(self.db)
+        self.applied_coupon_manager = AppliedCouponManager(self.db)
 
     async def _initialize_db(self):
         """Инициализация БД асинхронно."""
